5425d91f7bee46647bd0cd46214baa0b3e59930b
Ho Tien Vu <ho0001vu@gmail.com>
2015-06-21 02:41:34 +0800

    offload resize& upload image tasks to  children actors, spawn one actor for each file

--- rest/src/main/scala/com/htvu/instamua/rest/api/services/MediaService.scala
+++ rest/src/main/scala/com/htvu/instamua/rest/api/services/MediaService.scala
@@ -1,25 +1,33 @@
 package com.htvu.instamua.rest.api.services
 
-import akka.actor.ActorSystem
+import java.util.UUID
+
+import akka.actor.{Actor, ActorSystem, Props, Status}
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
 import awscala._
 import awscala.s3._
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.htvu.instamua.rest.Configs
 import com.htvu.instamua.rest.api.JsonFormats
 import com.htvu.instamua.rest.dao.{Image, Media}
-import spray.http.MultipartFormData
+import spray.http.{BodyPart, MultipartFormData}
 import spray.routing.Directives
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
-class MediaService()(implicit system: ActorSystem) extends Directives  with S3Service {
-  val routes = pathPrefix("listing" / "media") {
+class MediaService()(implicit system: ActorSystem) extends Directives {
+  val mediaCtlr = system.actorOf(Props(new MediaController()))
+  import scala.concurrent.duration._
+  implicit val timeout = Timeout(5.seconds)
+
+  val routes = pathPrefix(Segment / "media") { endPoint => // endPoint = 'listings' or 'users'
     path("upload") {
       post {
         entity(as[MultipartFormData]) { formData =>
           ctx => {
             implicit def marshaller = JsonFormats.json4sMarshaller
-            ctx.complete(uploadListingPhotos(formData))
+            ctx complete (mediaCtlr ? MediaController.FileUpload(endPoint, formData)).mapTo[List[Media]]
           }
         }
       }
@@ -29,10 +37,91 @@ class MediaService()(implicit system: ActorSystem) extends Directives  with S3Se
   implicit def executionContext: ExecutionContext = system.dispatcher
 }
 
-trait S3Service {
-  private val IMAGE_STANDARD_WIDTH = 320
-  private val IMAGE_STANDARD_HEIGHT = 320
+object MediaController {
+  def props: Props = Props(new MediaController())
+  case class FileUpload(endPoint: String, formData: MultipartFormData)
+}
 
+class MediaController() extends Actor {
+  val promises =  scala.collection.mutable.Map[String, Promise[List[Media]]]()
+  val done  = scala.collection.mutable.Map[String, List[Media]]()
+  val expectedNumParts = scala.collection.mutable.Map[String, Int]()
+
+  import MediaController._
+  import MediaUploader._
+
+  implicit def executionContext = context.dispatcher
+
+  def receive: Receive = {
+    case FileUpload(endPoint, data) =>
+      val p = Promise[List[Media]]()
+      val hash = UUID.randomUUID().toString
+      data.fields.foreach(part => context.actorOf(MediaUploader.props(endPoint, hash, part)))
+      promises += hash -> p
+      done += hash -> Nil
+      expectedNumParts += hash -> data.fields.size
+      p.future pipeTo sender
+    case UploadSuccess(h, x) =>
+      val p = promises(h)
+      val xs = x::done(h)
+      done += h -> xs
+      if (xs.size == expectedNumParts(h)) {
+        println("successful")
+        p.success(xs)
+        clear(h)
+      }
+    case UploadFailed(h, e) =>
+      val p = promises(h)
+      p.failure(e)
+      clear(h)
+  }
+
+  private def clear(h: String) = {
+    promises -= h
+    done -= h
+    expectedNumParts -= h
+  }
+}
+
+object MediaUploader {
+  def props(endPoint: String, uploadId: String, data: BodyPart): Props = Props(new MediaUploader(endPoint, uploadId, data))
+
+  case class UploadSuccess(uploadId: String, x: Media)
+  case class UploadFailed(uploadId: String, e: Throwable)
+}
+
+class MediaUploader(endPoint: String, uploadId: String, data: BodyPart) extends Actor with S3Service with MediaResize {
+  import MediaUploader._
+
+  val resizeUpload = for {
+    thumbnail <- resize(data.entity.data.toByteArray)
+    image <- uploadPhoto(endPoint, "thumbnail")(data.filename.get, thumbnail)
+  } yield image
+
+  val uploadStandard = uploadPhoto(endPoint, "standard")(data.filename.get, data.entity.data.toByteArray)
+
+  // combine 2 futures and return a future of uploaded Media
+  val futureMedia = Future.sequence(List(uploadStandard, resizeUpload)) map (l =>
+    Media(standard = Some(Image(l.head, 160, 160)), thumbnail = Some(Image(l(1), 80, 80))))
+
+  futureMedia pipeTo self
+
+  def receive: Actor.Receive = {
+    case x: Media =>
+      context.parent ! UploadSuccess(uploadId, x)
+      context.stop(self)
+    case Status.Failure(e) =>
+      context.parent ! UploadFailed(uploadId, e)
+  }
+
+  implicit def executionContext: ExecutionContext = context.dispatcher
+}
+
+trait MediaResize {
+  def resize(image: Array[Byte]): Future[Array[Byte]] = Future.successful(image)
+}
+
+trait S3Service {
   implicit val region = Region.AP_SOUTHEAST_1
   implicit val s3 = S3(Configs.config.getString("aws.access-key-id"), Configs.config.getString("aws.secret-access-key"))
 
@@ -41,23 +130,19 @@ trait S3Service {
 
   implicit def executionContext: ExecutionContext
 
-  def upload(formData: MultipartFormData, bucket: String): Future[Seq[Media]] = {
-    Future.sequence(formData.fields.map(part => Future {
+  def upload(bucket: String)(filename: String, data: Array[Byte]): Future[String] = {
+    Future {
       val meta = new ObjectMetadata()
-      meta.setContentLength(part.entity.data.length)
+      meta.setContentLength(data.length)
       s3.put(
         Bucket(s"$baseBucket/$bucket"),
-        part.filename.get,
-        part.entity.data.toByteArray,
+        filename,
+        data,
         meta
       )
-      Media(standard = Some(Image(s"$s3BaseUrl/$baseBucket/$bucket/${part.filename.get}", IMAGE_STANDARD_WIDTH, IMAGE_STANDARD_HEIGHT)))
-    }))
+      s"$s3BaseUrl/$baseBucket/$bucket/$filename"
+    }
   }
 
-  def uploadListingPhotos(formData: MultipartFormData): Future[Seq[Media]] =
-    upload(formData, "listings/standard")
-
-  def uploadUserProfilePicture(formData: MultipartFormData): Future[Media] =
-    upload(formData, "users/standard") map (_.head)
+  def uploadPhoto(endPoint: String, resolution: String): (String, Array[Byte]) => Future[String] = upload(s"$endPoint/$resolution")
 }
